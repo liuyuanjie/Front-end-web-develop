a17d2f5b5e82f6ebb78c76adbcee66d829131e3c
liuyuanjie <liuyj@shinetechchina.com>
2015-10-14 00:02:29 +0800

    test

--- JavaScript/20150112.js
+++ JavaScript/20150112.js
@@ -3,5 +3,96 @@
  */
 function test()
 {
-    alert('success!');
+    //object.getNameFunc();
+    //console.log(object.printNameFunc());
+    //object.name="uu111";
+    //object.printNameFunc();
+    //print();
 }
+
+var name = "The Window";
+var object = {
+    name :"My Object",
+    getNameFunc:function() {
+        this.name='kaka';
+    },
+    /*printNameFunc:function(){
+        return (function(a){
+            return a.name;
+        })(this);
+    }*/
+    printNameFunc:function(){
+        this.name='fun';
+        console.log(this.name);
+        return function(obj){
+            obj.name='fun_-_1';
+            console.log(obj.name);
+            console.log(this.name);
+        }(this);
+    }
+　};
+object.printNameFunc();
+console.log('_________________________________');
+
+function ctrip(){
+
+    ////this.name='this';
+    //(function plane(){
+    //    //name="third";
+    //      console.log(name+ '  plane');
+    //   })();
+    ////plane();
+    //var name="second";
+    //console.log(name+'log');
+    console.log(name+'log');
+    var name=0;
+   }
+
+ ctrip();
+ console.log(name);
+
+//function print(){
+//    console.log(name);
+//    name='333';
+//    console.log(name);
+//}
+/*var object1 = {
+    name : "second",
+    ctrip:function()
+    {
+        *//*this.name = "one";
+        function plane() {
+        name = "third";
+            console.log(this.name);
+        }
+
+        plane();*//*
+        console.log("dd"=="dd");
+        console.log(1==true);
+        var q;
+        console.log(q==null);
+        console.log(q==undefined);
+        console.log(' '==false);
+        console.log(' 1'==false);
+    }
+};
+object1.ctrip();*/
+//(function ctrip()
+//{
+//    /*this.name = "one";
+//     function plane() {
+//     name = "third";
+//     console.log(this.name);
+//     }
+//
+//     plane();*/
+//    console.log("dd"=="dd");
+//    console.log(1==true);
+//    var q;
+//    console.log(q==null);
+//    console.log(q==undefined);
+//    console.log(' '==false);
+//    console.log(' 1'==false);
+//})();
+//ctrip();
+console.log(name);
